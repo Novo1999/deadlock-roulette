ab420536fbf6b835320dc993adc2934fcd02e812
Novo1999 <neptune.navo@gmail.com>
2024-11-21 18:36:21 +0600

    update: user can lock heroes in place after rouletting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { deadlockLogo, heroes } from './data.js';
-
+import DownArrow from './DownArrow.jsx';
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -12,15 +12,41 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [playerCount, setPlayerCount] = useState(1);
   const [randomHeroIndices, setRandomHeroIndices] = useState(null)
-  console.log("🚀 ~ App ~ randomHeroIndices:", randomHeroIndices)
   const [alp, setAlp] = useState(defaultAlp)
   const [lockedHeroes, setLockedHeroes] = useState([])
-  console.log("🚀 ~ App ~ lockedHeroes:", lockedHeroes)
+  const [widthOfHeroSlots, setWidthOfHeroSlots] = useState("")
+
+  const handleRandomHeroAfterLock = async (heroes) => {
+    setIsLoading(true)
+    await sleep(800)
+    const newSet = new Set()
+    while (newSet.size !== playerCount - lockedHeroes.length) {
+      let randomIndex = Math.floor(Math.random() * heroes.length)
+      if (lockedHeroes.includes(randomIndex)) {
+        return handleRandomHeroAfterLock(heroes.filter((_, index) => index !== randomIndex))
+      }
+      newSet.add(randomIndex)
+    }
+    let indicesToIgnore = []
+    const existingRandomHeroes = Array.from(randomHeroIndices)
+    existingRandomHeroes.forEach((hero, index) => lockedHeroes.includes(hero) && indicesToIgnore.push(index))
+
+    const arrayOfNewSet = Array.from(newSet)
+    let newSetIndex = 0
+    existingRandomHeroes.forEach((_, index) => {
+      if (!indicesToIgnore.includes(index)) {
+        existingRandomHeroes[index] = arrayOfNewSet[newSetIndex]
+        newSetIndex++
+      }
+    })
+
+    setRandomHeroIndices(new Set(existingRandomHeroes))
+    setIsLoading(false)
+  }
 
   const handleRandomHero = async () => {
     setIsLoading(true)
     await sleep(800)
-    setIsLoading(false)
     setRandomHeroIndices(prev => {
       const newSet = new Set(prev)
       if (newSet.size > 0) newSet.clear()
@@ -30,6 +56,7 @@ function App() {
       }
       return newSet
     })
+    setIsLoading(false)
   };
 
   const handleSelectPlayerCount = (e) => {
@@ -38,30 +65,42 @@ function App() {
 
     setPlayerCount(count);
     setRandomHeroIndices(null)
+    setLockedHeroes([])
     const dropdownContent = document.querySelector(".dropdown-content");
     dropdownContent.style.display = "none";
   };
 
   const handleLockedHeroes = (val) => {
-    setLockedHeroes(prev => prev.includes(val) ? prev.filter(item => item !== val) : [...prev, val])
+    playerCount > 1 && !!randomHeroIndices && setLockedHeroes(prev => prev.includes(val) ? prev.filter(item => item !== val) : [...prev, val])
   }
+
+  useEffect(() => {
+    const heroSlots = document.querySelector(".hero-slots")
+    setWidthOfHeroSlots(heroSlots.clientWidth)
+  }, [playerCount])
 
   return (
     <div className='m-auto flex flex-col items-center text-center'>
-      <div className='flex gap-2 flex-wrap'>
+
+      {!!randomHeroIndices && playerCount > 1 && <p className={`absolute top-12 text-2xl bg-white p-1 rounded px-4 ${lockedHeroes.length === playerCount ? "text-red-500" : "text-black"}`}>{lockedHeroes.length === playerCount ? "What's the point of doing this?" : "Hover to Lock Heroes in Place"}</p>}
+      {playerCount > 1 && <div style={{ width: widthOfHeroSlots, transition: "ease-in", transitionDuration: "0.3s" }} className={`absolute top-32 border-2 border-b`}>
+      </div>}
+
+      <div className='flex gap-2 flex-wrap mt-36 relative hero-slots'>
         {
           (randomHeroIndices ? Array.from(randomHeroIndices) : Array(playerCount).fill(0)).map((val, index) => (
             <motion.div
               key={index}
               className='relative'
             >
+              {playerCount > 1 && randomHeroIndices && <DownArrow fill={lockedHeroes.includes(val) ? "yellow" : "lime"} className={`w-12 absolute bottom-36 left-12 ${randomHeroIndices && !lockedHeroes.includes(val) ? "animate-bounceLow" : ""}`} />}
               <motion.img
                 onClick={() => handleLockedHeroes(val)}
                 transition={{ duration: 0.7 }}
-                animate={{ rotateY: isLoading ? 180 : 0 }}
-                src={isLoading ? deadlockLogo : randomHeroIndices && randomHeroIndices.size > 0 ? heroes[val] : heroes[0]}
+                animate={{ rotateY: isLoading && !lockedHeroes.includes(val) ? 180 : 0 }}
+                src={isLoading && !lockedHeroes.includes(val) ? deadlockLogo : randomHeroIndices && randomHeroIndices.size > 0 ? heroes[val] : heroes[0]}
                 alt={`Hero ${index + 1}`}
-                className={`w-36 h-48 object-contain cursor-pointer hover:filter hover:grayscale ${lockedHeroes.includes(val) ? "filter grayscale" : ""}`}
+                className={`w-36 h-48 object-contain cursor-pointer ${playerCount > 1 ? "hover:filter hover:grayscale" : ""} ${lockedHeroes.includes(val) ? "filter grayscale" : ""}`}
               />
               <div className="badge badge-primary p-6 text-xl font-bold uppercase mt-2">{alp[index]}</div>
               {lockedHeroes.includes(val) && <div onClick={() => handleLockedHeroes(val)} className='absolute top-16 left-0 right-0 bottom-0 text-3xl z-[50] m-auto cursor-pointer'>🔒</div>}
@@ -94,7 +133,7 @@ function App() {
             }
           </ul>
         </div>
-        <button disabled={isLoading} onClick={handleRandomHero} className='btn btn-primary'>Give Random Heroes</button>
+        <button disabled={isLoading || lockedHeroes.length === playerCount} onClick={lockedHeroes.length > 0 ? () => handleRandomHeroAfterLock(heroes) : handleRandomHero} className='btn btn-primary'>Give Random Heroes</button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
-import daisyui from 'daisyui';
+import daisyui from 'daisyui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  daisyui: {
-    themes: ["aqua"]
-  },
-  plugins: [daisyui],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            keyframes: {
+                bounceLow: {
+                    '0%, 100%': { transform: 'translateY(-5px)' },
+                    '50%': { transform: 'translateY(0)' },
+                },
+            },
+            animation: {
+                bounceLow: 'bounceLow 1s infinite',
+            },
+        },
+    },
+    daisyui: {
+        themes: ['aqua'],
+    },
+    plugins: [daisyui],
 }
